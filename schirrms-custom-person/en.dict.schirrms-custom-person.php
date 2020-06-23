cf555dfd9e821c97fd9ccfdb44293875c5a1b833
Schirrms <schirrms@schirrms.net>
2020-06-23 18:40:48 +0200

    Initial Version (0.0.1.0)

---
+++ schirrms-custom-person/en.dict.schirrms-custom-person.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2020 Schirrms
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Class:Person/UniquenessRule:S_employee_number+' => 'The employee number must be unique in the organization',
+	'Class:Person/UniquenessRule:S_employee_number' => 'there is already a person in \'$this->org_name$\' organization with the same employee number',
+));
+?>
---
+++ schirrms-custom-person/fr.dict.schirrms-custom-person.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2020 Schirrms
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Class:Person/UniquenessRule:S_employee_number+' => 'Le numéro d\'employé doit être unique dans l\'organisation',
+	'Class:Person/UniquenessRule:S_employee_number' => 'il y a déjà une personne avec ce numéro d\'employé dans l\'organisation \'$this->org_name$\'',
+));
+?>
